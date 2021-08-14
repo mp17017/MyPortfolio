629f03483c6260f9332be66d04e11d64c914177b
Michael Perry <michaeleperr@gmail.com>
2021-08-14 14:55:58 -0400

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //app.use(express.static(__dirname + '/public'));
-import 'style.css';
+import '3dPortfolio\dist\style.css';
 //import * as THREE from 'three';
 import * as THREE from 'https://threejs.org/build/three.module.js';
 //import { BlendingEquation } from 'three';
